1fc8dedd1ce0cc0f14dd1965666707d46a363b05
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-10-01 17:22:05 +0200

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,28 @@
-const getLabel = document.querySelectorAll(`label`);
-console.log(getLabel);
+
+
+const getInput = document.querySelector(`input`);
+const getText = document.querySelector(`textarea`);
+
+const object = {
+    getInput,
+    getText
+};
+
+getInput.addEventListener(`input`, (event)=>{
+    console.log(event.currentTarget.value);
+    localStorage.setItem("feedback-form-state", event.currentTarget.value)
+})
+
+
+const getForm = document.querySelector(`form`);
+
+const theme = localStorage.getItem("feedback-form-state");
+getInput.value = getInput.value + `${theme}`
+
+console.log(theme);
+
+getForm.addEventListener(`submit`, (event)=>{
+    localStorage.clear(" ");
+    
+    
+});
