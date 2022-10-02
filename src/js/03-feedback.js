dd00b021009cd5bb787ce8226a28ff1b4aeb5e28
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-10-02 08:08:06 +0200

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,28 +1,60 @@
-
+import throttle from 'lodash.throttle';
 
 const getInput = document.querySelector(`input`);
 const getText = document.querySelector(`textarea`);
 
-const object = {
-    getInput,
-    getText
-};
-
-getInput.addEventListener(`input`, (event)=>{
-    console.log(event.currentTarget.value);
-    localStorage.setItem("feedback-form-state", event.currentTarget.value)
-})
-
+const object = {};
 
 const getForm = document.querySelector(`form`);
 
-const theme = localStorage.getItem("feedback-form-state");
-getInput.value = getInput.value + `${theme}`
+getForm.addEventListener(`input`, throttle(onInput, 500));
 
-console.log(theme);
+function onInput(event){
+    object[event.target.name] = event.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(object))
+};
+
+getObject = localStorage.getItem("feedback-form-state");
+
+parsedObject = JSON.parse(getObject);
+
+function setValue(){
+    getInput.value = parsedObject.email;
+    getText.value = parsedObject.message;
+};
+
+function clearValue(){
+    getInput.value = "";
+    getText.value = "";
+}
+setValue();
+console.log(getObject);
+console.log(parsedObject);
 
 getForm.addEventListener(`submit`, (event)=>{
-    localStorage.clear(" ");
-    
+    console.log(parsedObject);
+    event.preventDefault();
+    clearValue();
     
 });
+
+
+
+// getInput.addEventListener(`input`, (event)=>{
+//     console.log(event.currentTarget.value);
+//     localStorage.setItem("feedback-form-state", event.currentTarget.value)
+// })
+
+
+// const getForm = document.querySelector(`form`);
+
+// const theme = localStorage.getItem("feedback-form-state");
+// getInput.value = getInput.value + `${theme}`
+
+// console.log(theme);
+
+// getForm.addEventListener(`submit`, (event)=>{
+//     localStorage.clear();
+    
+    
+// });
