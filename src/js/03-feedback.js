f57fc1f884db9d6a4ac33f7a5c8488c73176fdfd
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-10-06 06:43:42 +0200

    update task 3 try

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,33 +4,31 @@ import throttle from 'lodash.throttle';
 const getInput = document.querySelector(`input`);
 const getText = document.querySelector(`textarea`);
 
-
 const object = {
-    email: getInput.value ,
-    message: getText.value 
+    email: ``,
+    message: `` 
     
 };
 
 
 const getForm = document.querySelector(`form`);
-// 
+
 getInput.addEventListener(`input`, throttle(onInput, 500));
 getText.addEventListener(`input`,throttle(onInput2,500));
 
 function onInput2(event){
-    // object.email = event.target.value;
     object.message=event.target.value;
     localStorage.setItem("feedback-form-state", JSON.stringify(object))
     console.log(object);
+    
+
 };
-
-
 
 function onInput(event){
     object.email = event.target.value;
-    // object.message=event.target.value;
     localStorage.setItem("feedback-form-state", JSON.stringify(object))
     console.log(object);
+    
 };
 
 const getObject = localStorage.getItem("feedback-form-state");
@@ -40,16 +38,6 @@ function setValue(){
     getInput.value = parsedObject.email !== undefined ? getInput.value= parsedObject.email : "";
     getText.value = parsedObject.message !== undefined ? getText.value= parsedObject.message: "";
 
-    // if (parsedObject.email !== undefined,
-    //     parsedObject.message !==undefined
-    //     ){
-    //     getInput.value = parsedObject.email,
-    //     getText.value = parsedObject.message
-        
-    // }else {
-    // getInput.value=parsedObject.email;
-    // getText.value= "";
-// }
 };
 
 setValue();
