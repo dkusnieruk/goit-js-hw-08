bd69e29f5cce0c6e8f7f7d7a936c74c472346daf
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-10-06 04:31:09 +0200

    update next try

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,19 +5,33 @@ const getInput = document.querySelector(`input`);
 const getText = document.querySelector(`textarea`);
 
 
-const object = {};
-
-const getForm = document.querySelector(`form`);
-// 
-getForm.addEventListener(`input`, throttle(onInput, 500));
-
-function onInput(event){
-    object[event.target.name] = event.target.value;
-    localStorage.setItem("feedback-form-state", JSON.stringify(object))
+const object = {
+    email: getInput.value ,
+    message: getText.value 
     
 };
 
 
+const getForm = document.querySelector(`form`);
+// 
+getInput.addEventListener(`input`, throttle(onInput, 500));
+getText.addEventListener(`input`,throttle(onInput2,500));
+
+function onInput2(event){
+    // object.email = event.target.value;
+    object.message=event.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(object))
+    console.log(object);
+};
+
+
+
+function onInput(event){
+    object.email = event.target.value;
+    // object.message=event.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(object))
+    console.log(object);
+};
 
 const getObject = localStorage.getItem("feedback-form-state");
 const parsedObject = JSON.parse(getObject);
@@ -48,9 +62,10 @@ function clearValue(){
 
 getForm.addEventListener(`submit`, (event)=>{
     event.preventDefault();
-    console.log(parsedObject);
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
     localStorage.clear();    
     clearValue();
+    parsedObject.clear;
 
 
 });
